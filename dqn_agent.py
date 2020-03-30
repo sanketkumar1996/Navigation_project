f3b9c08f6ebdd8c9240ce528cf3a2ace5b286989
Sanket Thakur <sanketsans@Sankets-MacBook-Pro.local>
2020-03-31 03:48:16 +0530

    modified_agents

--- dqn_agent.py
+++ dqn_agent.py
@@ -2,9 +2,10 @@ import numpy as np
 import random
 from collections import namedtuple, deque
 
-from model import QNetwork
+from model import Model
 
 import torch
+import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 
@@ -15,116 +16,72 @@ TAU = 1e-3              # for soft update of target parameters
 LR = 5e-4               # learning rate (Initially 5e-4)
 UPDATE_EVERY = 4        # how often to update the network
 
-# device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-device = torch.device("cpu")
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-class Agent():
-    """Interacts with and learns from the environment."""
+class Agent:
+    def __init__(self, state_size, action_size):
+        self.epsilon = 1.0
+        self.min_eps = 0.05
+        self.decay = 0.995
+        self.seed = random.seed(42)
 
-    def __init__(self, state_size, action_size, seed):
-        """Initialize an Agent object.
-
-        Params
-        ======
-            state_size (int): dimension of each state
-            action_size (int): dimension of each action
-            seed (int): random seed
-        """
         self.state_size = state_size
         self.action_size = action_size
-        self.seed = random.seed(seed)
 
-        # Q-Network
-        self.qnetwork_local = QNetwork(state_size, action_size, seed).to(device)
-#         self.qnetwork_target = QNetwork(state_size, action_size, seed).to(device)
-        self.qnetwork_local.load_state_dict(torch.load('/Users/sanketsans/Documents/Udacity/deepRL/deep-reinforcement-learning/p1_navigation/checkpoint.pth', map_location=torch.device('cpu')))
+        self.local_nn = Model(state_size, action_size).to(device)
+        # self.target_nn = Model(state_size, action_size).to(device)
+        self.qnetwork_local.load_state_dict(torch.load('/Users/sanketsans/Documents/Udacity/deepRL/deep-reinforcement-learning/p1_navigation/checkpoint0.pth', map_location=torch.device('cpu')))
         self.qnetwork_local.eval()
 
-        self.optimizer = optim.Adam(self.qnetwork_local.parameters(), lr=LR)
+        self.optimizer = optim.Adam(self.local_nn.parameters(), lr=LR)
 
-        # Replay memory
-        self.memory = ReplayBuffer(action_size, BUFFER_SIZE, BATCH_SIZE, seed)
-        # Initialize time step (for updating every UPDATE_EVERY steps)
+        self.memory = ReplayMemory(BUFFER_SIZE, action_size)
+
         self.t_step = 0
 
-    def step(self, state, action, reward, next_state, done):
-        # Save experience in replay memory
-        self.memory.add(state, action, reward, next_state, done)
+    def step(self, state, action, reward, new_state, done):
+        ## store experiences in replay buffer
+        self.memory.add(state, action, reward, new_state, done)
 
-        # Learn every UPDATE_EVERY time steps.
+        ##learn every update_every seconds
         self.t_step = (self.t_step + 1) % UPDATE_EVERY
         if self.t_step == 0:
-            # If enough samples are available in memory, get random subset and learn
-            if len(self.memory) > BATCH_SIZE:
-                experiences = self.memory.sample()
+            if (self.memory.length()) > BATCH_SIZE:
+                experiences = self.memory.sample(BATCH_SIZE)
                 self.learn(experiences, GAMMA)
 
-    def act(self, state, eps=0.):
-        """Returns actions for given state as per current policy.
-
-        Params
-        ======
-            state (array_like): current state
-            eps (float): epsilon, for epsilon-greedy action selection
-        """
+    def act(self, state):
         state = torch.from_numpy(state).float().unsqueeze(0).to(device)
-        # self.qnetwork_local.eval()
+        self.local_nn.eval()
         with torch.no_grad():
-            action_values = self.qnetwork_local(state)
-        # self.qnetwork_local.train()
+            action_values = self.local_nn(state)
+        self.local_nn.train()
 
-        # Epsilon-greedy action selection
-        if random.random() > eps:
+        self.epsilon = max(self.epsilon*self.decay, self.min_eps)
+
+        if random.random() > self.epsilon:
             return np.argmax(action_values.cpu().data.numpy())
         else:
             return random.choice(np.arange(self.action_size))
 
     def learn(self, experiences, gamma):
-        """Update value parameters using given batch of experience tuples.
-
-        Params
-        ======
-            experiences (Tuple[torch.Tensor]): tuple of (s, a, r, s', done) tuples
-            gamma (float): discount factor
-        """
+        # Get max predicted Q values (for next states) from target model
         states, actions, rewards, next_states, dones = experiences
 
-        ## TODO: compute and minimize the loss
-        "*** YOUR CODE HERE ***"
-
-        # Get max predicted Q values for next states from the target model (frozen weights)
-        #
-        #    next_states is 64x8
-        #    self.qnetwork_target(next_states) is 64x4
-        #    detach() returns a tensor copy detached from the graph (no gradient)
-        #    max(1)[0] returns the the max value in given dim  (max value indexes in 2nd array)
-        #    => This returns an array of 64 values
-        #    Unsqueeze(1)returns a new Tensor of size one inserted at the given position
-        #    => This returns a 64X1 tensor
-        Q_targets_next = self.qnetwork_target(next_states).detach().max(1)[0].unsqueeze(1)
-
-        # Compute Q targets for current states
+        Q_targets_next = self.target_nn(next_states).detach().max(1)[0].unsqueeze(1)
         Q_targets = rewards + (gamma * Q_targets_next * (1 - dones))
 
-        # Get expected Q values from local model (being trained)
-        # x.gather(1, actions) returns a tensor (located on the current device) that is the result of
-        # concataining the input tensor values along the provided dimensions (here the dim indexes are the taken actions indexes)
-        Q_expected = self.qnetwork_local(states).gather(1, actions)
+        Q_expected = self.local_nn(states).gather(1, actions)
 
         # Compute loss
         loss = F.mse_loss(Q_expected, Q_targets)
-
         # Minimize the loss
         self.optimizer.zero_grad()
         loss.backward()
         self.optimizer.step()
 
-
         # ------------------- update target network ------------------- #
-        self.soft_update(self.qnetwork_local, self.qnetwork_target, TAU)
-
-
-
+        self.soft_update(self.local_nn, self.target_nn, TAU)
 
     def soft_update(self, local_model, target_model, tau):
         """Soft update model parameters.
@@ -140,33 +97,25 @@ class Agent():
             target_param.data.copy_(tau*local_param.data + (1.0-tau)*target_param.data)
 
 
-class ReplayBuffer:
-    """Fixed-size buffer to store experience tuples."""
 
-    def __init__(self, action_size, buffer_size, batch_size, seed):
+
+class ReplayMemory:
+
+    def __init__(self, buffer_size, action_size):
         """Initialize a ReplayBuffer object.
 
-        Params
-        ======
-            action_size (int): dimension of each action
-            buffer_size (int): maximum size of buffer
-            batch_size (int): size of each training batch
-            seed (int): random seed
-        """
         self.action_size = action_size
         self.memory = deque(maxlen=buffer_size)
-        self.batch_size = batch_size
         self.experience = namedtuple("Experience", field_names=["state", "action", "reward", "next_state", "done"])
-        self.seed = random.seed(seed)
+        self.seed = random.seed(42)
 
     def add(self, state, action, reward, next_state, done):
         """Add a new experience to memory."""
         e = self.experience(state, action, reward, next_state, done)
         self.memory.append(e)
 
-    def sample(self):
-        """Randomly sample a batch of experiences from memory."""
-        experiences = random.sample(self.memory, k=self.batch_size)
+    def sample(self, batch_size):
+        experiences = random.sample(self.memory, k=batch_size)
 
         states = torch.from_numpy(np.vstack([e.state for e in experiences if e is not None])).float().to(device)
         actions = torch.from_numpy(np.vstack([e.action for e in experiences if e is not None])).long().to(device)
@@ -176,6 +125,6 @@ class ReplayBuffer:
 
         return (states, actions, rewards, next_states, dones)
 
-    def __len__(self):
+    def length(self):
         """Return the current size of internal memory."""
         return len(self.memory)
--- model.py
+++ model.py
@@ -5,34 +5,22 @@ import torch.nn.functional as F
 class QNetwork(nn.Module):
     """Actor (Policy) Model."""
 
-    def __init__(self, state_size, action_size, seed):
-        """Initialize parameters and build model.
-        Params
-        ======
-            state_size (int): Dimension of each state
-            action_size (int): Dimension of each action
-            seed (int): Random seed
-        """
+    def __init__(self, state_size, action_size):
         super(QNetwork, self).__init__()
-        self.seed = torch.manual_seed(seed)
+        self.seed = torch.manual_seed(42)
         "*** YOUR CODE HERE ***"
-        
+
         fc1_units = 1024 #256
         fc2_units = 1024 #256
-        
+
         self.fc1 = nn.Linear(state_size, fc1_units)
         self.fc2 = nn.Linear(fc1_units, fc2_units)
         self.fc3 = nn.Linear(fc2_units, action_size)
-        
-  
+
+
     def forward(self, state):
-        """Build a network that maps state -> action values."""
-        
-        x = F.relu(self.fc1(state))
-        #x = F.dropout(x, p=0.0, training=self.training)
-        x = F.relu(self.fc2(x))
-        #x = F.dropout(x, p=0.0, training=self.training)
-        x = self.fc3(x)
-        
-        return x
-        
+        state = F.relu(self.fc1(state))
+        state = F.relu(self.fc2(state))
+        action = self.fc3(state)
+
+        return action
